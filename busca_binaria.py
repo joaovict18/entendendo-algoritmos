7e2a9224fe6d9ac27fe4a8d0d1f6ac0320d6393c
Victor <jovimedim@gmail.com>
2025-02-21 00:33:41 -0300

    pesquisa binaria

---
+++ busca_binaria.py
@@ -0,0 +1,21 @@
+def pesquisa_binaria(lista, item):
+    baixo = 0
+    alto = len(lista) - 1
+
+    while baixo <= alto:
+        meio = (baixo + alto) / 2
+        chute = lista[meio]
+        if chute == item:
+            return meio
+        if chute > item:
+            alto = meio - 1
+        else:
+            baixo = meio + 1
+    return None
+
+minha_lista = [1, 3, 4.5, 5, 7, 9]
+
+print(pesquisa_binaria(minha_lista, 3))
+print(pesquisa_binaria(minha_lista, -1))
+
+# o algoritmo acima só funciona para valores inteiros e se a lista estiver ordenada
